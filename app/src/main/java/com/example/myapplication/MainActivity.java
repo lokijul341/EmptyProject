e43dfdb91ada0b21ebfc36fb851605157c55d711
User <rnltmst@mail.ru>
2023-02-16 23:36:49 +0300

    Добавили уведомления

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,12 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e(TAG, "Ошибка в методе onCreate");
-        Log.w(TAG, "Предупреждение в методе onCreate");
-        Log.i(TAG, "Информация в методе onCreate");
-        Log.d(TAG, "Откладка в методе onCreate");
-        Log.v(TAG, "Подробности в методе onCreate");
-
 
     }
     @Override
@@ -35,11 +29,7 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
         super.onStart();
-        Log.e(TAG, "Ошибка в методе onStart");
-        Log.w(TAG, "Предупреждение в методе onStart");
-        Log.i(TAG, "Информация в методе onStart");
-        Log.d(TAG, "Откладка в методе onStart");
-        Log.v(TAG, "Подробности в методе onStart");
+
     }
     @Override
     protected void onStop() {
@@ -48,11 +38,7 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
         super.onStop();
-        Log.e(TAG, "Ошибка в методе onStop");
-        Log.w(TAG, "Предупреждение в методе onStop");
-        Log.i(TAG, "Информация в методе onStop");
-        Log.d(TAG, "Откладка в методе onStop");
-        Log.v(TAG, "Подробности в методе onStop");
+
    }
     @Override
     protected void onDestroy() {
@@ -61,11 +47,7 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
         super.onDestroy();
-        Log.e(TAG, "Ошибка в методе onDestroy");
-        Log.w(TAG, "Предупреждение в методе onDestroy");
-        Log.i(TAG, "Информация в методе onDestroy");
-        Log.d(TAG, "Откладка в методе onDestroy");
-        Log.v(TAG, "Подробности в методе onDestroy");
+
     }
     @Override
     protected void onPause() {
@@ -74,11 +56,7 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
         super.onPause();
-        Log.e(TAG, "Ошибка в методе onPause");
-        Log.w(TAG, "Предупреждение в методе onPause");
-        Log.i(TAG, "Информация в методе onPause");
-        Log.d(TAG, "Откладка в методе onPause");
-        Log.v(TAG, "Подробности в методе onPause");
+
     }
     @Override
     protected void onResume() {
@@ -87,10 +65,6 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context, text, duration).show();
         super.onResume();
-        Log.e(TAG, "Ошибка в методе onResume");
-        Log.w(TAG, "Предупреждение в методе onResume");
-        Log.i(TAG, "Информация в методе onResume");
-        Log.d(TAG, "Откладка в методе onResume");
-        Log.v(TAG, "Подробности в методе onResume");
+
     }
 }
